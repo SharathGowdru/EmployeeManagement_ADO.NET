e6b3a3aea3a2b921dbf2712e607d694b728e27ad
Sharath <Sharus9587@gmail.com>
2021-06-10 07:04:13 +0530

    Added : Error in the program,test cases isn't passing

---
+++ EmployeeManagement/EmployeeManagementTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmployeeManagementTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmployeeManagementTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("23318211-98c1-49de-934c-ba8a9e313aca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EmployeeManagement/EmployeeManagementTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using EmployeeManagement;
+using EmployeeManagement.Model.SalaryModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace EmployeeManagementTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenSalaryDataAbleToUpdateSalaryDetails()
+        {
+            Salary salary = new Salary();
+            SalaryUpdateModel updateModel = new SalaryUpdateModel()
+            {
+                SalaryId = 1,
+                SalaryMonth = "Jan",
+                EmployeeSalary = 1300,
+                EmployeeId = 2
+            };
+
+            int EmpSalary = salary.UpdateEmpSal(updateModel);
+
+            Assert.AreEqual(updateModel.EmployeeSalary, EmpSalary);
+        }
+    }
+
+}
